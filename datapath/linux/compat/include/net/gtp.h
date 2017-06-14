888dc2996bb75203dd60f5ed7724de1295b9de0d
Jiannan Ouyang <ouyangj@fb.com>
2017-06-14 22:28:11 +0000

    datapath: GPRS Tunneling Protocol (GTP) support
    Signed-off-by: Jiannan Ouyang <ouyangj@fb.com>

---
+++ datapath/linux/compat/include/net/gtp.h
@@ -0,0 +1,35 @@
+#ifndef __NET_GTP_WRAPPER_H
+#define __NET_GTP_WRAPPER_H  1
+
+#ifdef CONFIG_INET
+#include <net/udp_tunnel.h>
+#endif /* ifdef CONFIG_INET */
+
+#ifdef USE_UPSTREAM_TUNNEL
+#include_next <net/gtp.h>
+
+static inline int rpl_gtp_init_module(void)
+{
+	return 0;
+}
+static inline void rpl_gtp_cleanup_module(void)
+{}
+
+#define gtp_xmit dev_queue_xmit
+
+#ifdef CONFIG_INET
+#ifndef HAVE_NAME_ASSIGN_TYPE
+static inline struct net_device *rpl_gtp_dev_create_fb(
+	struct net *net, const char *name, u8 name_assign_type, u16 dst_port) {
+	return gtp_dev_create_fb(net, name, dst_port);
+}
+#define gtp_dev_create_fb rpl_gtp_dev_create_fb
+#endif
+#endif
+
+#endif /* USE_UPSTREAM_TUNNEL */
+
+#define gtp_init_module rpl_gtp_init_module
+#define gtp_cleanup_module rpl_gtp_cleanup_module
+
+#endif /* __NET_GTP_WRAPPER_H */
